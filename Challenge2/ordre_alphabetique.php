9c4ad9aab348482c1f0cefa402443143e83eec80
Lauura <laura.grabot@aliceadsl.fr>
2017-02-10 13:46:44 +0100

    Update Chall 2
    Modification du script ordre_alphabetique.php
    Ajout de la page HTML d’indice

--- Challenge2/ordre_alphabetique.php
+++ Challenge2/ordre_alphabetique.php
@@ -1,8 +1,4 @@
 <?php
-//$ligne = file($_SERVER['argv'][1]);
-//$ligne = file("dico.txt");
-//sort($ligne);
-
 $lines = file($_SERVER['argv'][1]);
 natsort($lines);
 file_put_contents($_SERVER['argv'][1], implode("", $lines));
